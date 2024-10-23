f19368840e99cd288812e684df0a2253c3577c0b
Ismail Mellali <imellali@c3r4p6.1337.ma>
2024-10-23 19:16:04 +0100

    tabs

--- libft/ft_isalnum.c
+++ libft/ft_isalnum.c
@@ -1,4 +1,4 @@
-int ft_isalpha(int c)
+int	ft_isalpha(int c)
 {
 	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 		return 1;
@@ -6,7 +6,7 @@ int ft_isalpha(int c)
 		return 0;
 }
 
-int ft_isdigit(int c)
+int	ft_isdigit(int c)
 {
 	if (c >= '0' && c <= '9')
 		return 1;
@@ -14,7 +14,7 @@ int ft_isdigit(int c)
 		return 0;
 }
 
-int ft_isalnum(int c)
+int	ft_isalnum(int c)
 {
 	if(ft_isalpha(c) || ft_isdigit(c))
 		return 1;
