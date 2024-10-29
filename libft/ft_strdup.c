bc44f5399a0f59cda0f4109665df482bdedffde9
Liamsi <mmeellaallii1@gmail.com>
2024-10-29 12:59:25 +0100

    mallocing

---
+++ libft/ft_strdup.c
@@ -0,0 +1,32 @@
+size_t ft_strlen(const char *s)
+{
+	size_t len;
+
+	len = 0;
+	while (s[len] != '\0')
+		len++;
+	return len;
+}
+
+char *ft_strdup(const char *s)
+{
+	size_t len;
+	size_t i;
+	char *buffer;
+
+	if (s == NULL)
+		return NULL;
+	i = 0;
+	len = ft_strlen(s);
+	
+	buffer = malloc(sizeof(char) * len);
+	if (buffer == NULL)
+		return NULL;
+	while (s[i] != '\0')
+	{
+		buffer[i] = s[i];
+		i++;
+	}
+	buffer[i] = '\0';
+	return buffer;
+}
